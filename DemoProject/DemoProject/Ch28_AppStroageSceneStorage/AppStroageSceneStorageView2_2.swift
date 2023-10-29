345416487d1ee87cd9ee3065950f8ce99c2cfe4d
Jihunkim95 <darak551@naver.com>
2023-10-29 12:31:58 +0900

    데이터 동시성 예제 학습

---
+++ DemoProject/DemoProject/Ch28_AppStroageSceneStorage/AppStroageSceneStorageView2_2.swift
@@ -0,0 +1,18 @@
+//
+//  AppStroageSceneStorageView2_2.swift
+//  DemoProject
+//
+//  Created by 김지훈 on 2023/10/29.
+//
+
+import SwiftUI
+
+struct AppStroageSceneStorageView2_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStroageSceneStorageView2_2()
+}
---
+++ DemoProject/DemoProject/Ch28_AppStroageSceneStorage/AppStroageSceneStorageView4.swift
@@ -0,0 +1,18 @@
+//
+//  AppStroageSceneStorageView4.swift
+//  DemoProject
+//
+//  Created by 김지훈 on 2023/10/29.
+//
+
+import SwiftUI
+
+struct AppStroageSceneStorageView4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStroageSceneStorageView4()
+}
